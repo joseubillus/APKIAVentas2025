144f85c632cc414459461483460a386f6168582e
joseubillus <jose_ubillus@hotmail.com>
2025-03-08 09:15:37 -0500

    commit1

--- app/src/main/java/com/example/apkventas/MnProducto.kt
+++ app/src/main/java/com/example/apkventas/MnProducto.kt
@@ -1,12 +1,18 @@
 package com.example.apkventas
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.SearchView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MnProducto : AppCompatActivity() {
+    private lateinit var lstpro:ListView
+    private lateinit var schbus:SearchView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +22,15 @@ class MnProducto : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        schbus = findViewById(R.id.FrmProd_SchBus)
+        lstpro = findViewById(R.id.FrmProd_LstProd)
+        var adp = ArrayAdapter<String>(this,
+            android.R.layout.simple_list_item_1)
+        adp.add("Producto 1")
+        adp.add("Producto 2")
+        adp.add("Producto 3")
+        adp.add("Producto 4")
+        lstpro.adapter = adp
     }
 }
