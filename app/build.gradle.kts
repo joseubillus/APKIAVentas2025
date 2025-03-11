6640507fed12674c803ba301d2da8c56efa6ae70
joseubillus <jose_ubillus@hotmail.com>
2025-03-11 17:04:48 -0500

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.apkventas"
-    compileSdk = 35
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.apkventas"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
