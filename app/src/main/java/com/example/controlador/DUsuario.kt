3b4e87833bc76a3bb6fa36066d5f0a550fa08255
joseubillus <jose_ubillus@hotmail.com>
2025-03-22 08:52:26 -0500

    commit

--- app/src/main/java/com/example/controlador/DUsuario.kt
+++ app/src/main/java/com/example/controlador/DUsuario.kt
@@ -1,7 +1,11 @@
 package com.example.controlador
 
 import android.content.Context
+import android.content.Intent
 import android.widget.Toast
+import com.example.apkventas.MnMenu
+import com.example.apkventas.MnProducto
+import com.example.util.Mensaje
 import com.loopj.android.http.AsyncHttpClient
 import com.loopj.android.http.AsyncHttpResponseHandler
 import com.loopj.android.http.RequestParams
@@ -12,14 +16,10 @@ import org.json.JSONObject
 
 class DUsuario(val c:Context) {
     private var asyn:AsyncHttpClient = AsyncHttpClient()
-    private var url:String  = "http://172.56.0.238:8080/login/"
+    private var url:String  = "http://192.168.18.4:8080/login/"
+    private var ms: Mensaje = Mensaje(c)
 
     fun getValidar(nom:String,pas:String):String {
-        /*var para:RequestParams = RequestParams()
-        para.add("id","0")
-        para.add("nombre_usuario",nom)
-        para.add("contrasena",pas)*/
-
         val jsonObject = JSONObject()
         jsonObject.put("id",0);
         jsonObject.put("nombre_usuario",nom);
@@ -27,16 +27,24 @@ class DUsuario(val c:Context) {
         val entity = StringEntity(jsonObject.toString());
 
         asyn.post(c,url,entity,"application/json", object:AsyncHttpResponseHandler(){
-        //asyn.get(c,url,entity,"application/json", object:AsyncHttpResponseHandler(){
-            override fun onSuccess(statusCode: Int,headers: Array<out Header>?,
-                responseBody: ByteArray?) {
+            override fun onStart() {
+                super.onStart()
+                ms!!.MProgressBarDato()
+            }
+
+            override fun onSuccess(statusCode: Int, headers: Array<out Header>?,
+                                   responseBody: ByteArray?) {
+                ms.MCloseProgBar(true)
                 val resp:String = java.lang.String(responseBody).toString()
-                Toast.makeText(c,"Resp:$resp",Toast.LENGTH_LONG).show()
+                val auto = JSONObject(resp).getString("mensaje")
+                if(auto.equals("Autorizado"))
+                    c.startActivity(Intent(c, MnProducto::class.java))
+                else Toast.makeText(c,"Resp:$auto",Toast.LENGTH_LONG).show()
             }
 
             override fun onFailure(statusCode: Int,headers: Array<out Header>?,
                 responseBody: ByteArray?,error: Throwable?) {
-
+                ms.MCloseProgBar(true)
                 if(responseBody!=null) {
                     val responseString = String(responseBody) // Convertir el responseBody a String
                     val jsonResponse = JSONObject(responseString) // Convertir a JSON
