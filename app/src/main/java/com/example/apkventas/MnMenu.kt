4f35c8729003c36a2bb2f707dc2246d6806855cd
joseubillus <jose_ubillus@hotmail.com>
2025-03-25 17:52:40 -0500

    commit

--- app/src/main/java/com/example/apkventas/MnMenu.kt
+++ app/src/main/java/com/example/apkventas/MnMenu.kt
@@ -5,8 +5,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.viewpager2.widget.ViewPager2
+import com.example.apkventas.menu.FragmePadre
+import com.google.android.material.tabs.TabLayout
 
 class MnMenu : AppCompatActivity() {
+    private lateinit var tabPadre:TabLayout
+    private lateinit var viewP:ViewPager2
+    private lateinit var fragPadre:FragmePadre
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +23,12 @@ class MnMenu : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        tabPadre = findViewById(R.id.FrmMenu_TabLayout)
+        viewP = findViewById(R.id.FrmMenu_ViewPage)
+        fragPadre = FragmePadre(supportFragmentManager,
+                    lifecycle,
+                    tabPadre.tabCount)
+        viewP.adapter = fragPadre
     }
 }
